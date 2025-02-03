173571e4d8a2f7e147b9b0c9f2c837d176549428
Lloyd <lloydleagas@gmail.com>
2025-02-03 10:08:07 +0200

    feature/entity

--- lib/entity.py
+++ lib/entity.py
@@ -1,5 +1,6 @@
 import pygame
 from util.maths import compute_total_acceleration, compute_velocity_vector, compute_position_vector, compute_total_impulse
+from util.util import line_length
 
 
 class Entity:
@@ -10,24 +11,32 @@ class Entity:
     acceleration: list
     velocity: list
 
-    def __init__(self, screen, position, mass):
+    def __init__(self, screen, position, mass, radius, field):
         self.screen = screen
         self.mass = mass
-        self.radius = mass
-        self.forces = [
-            [20, 90]
-        ]
+        self.radius = radius
+        self.forces = []
         self.impulse = []
         self.acceleration = [0, 0],
         self.velocity = [0, 0]
         self.position = position
-        self.field = mass * 20
+        self.field = field
 
-    def apply_force(self, force):
-        self.forces.append(force)
+    def apply_force(self, entities):
+        forces = []
+        for e in entities:
+            if e != self:
+                distance = line_length(self.position, e.position)
+                if distance < e.field:
+                    forces.append([20, 90])
+        self.forces = forces
 
-    def apply_impulse(self, force, angle):
-        self.impulse.append(force, angle)
+    def apply_impulse(self, entities):
+        for e in entities:
+            if e != self:
+                distance = line_length(self.position, e.position)
+                if distance < self.radius + e.radius:
+                    self.impulse.append([200, -90])
 
     def compute_force(self, frame_time):
         self.acceleration = compute_total_acceleration(
@@ -37,14 +46,17 @@ class Entity:
         self.position = compute_position_vector(
             self.position, self.velocity, frame_time)
 
-    def compute_impulse(self):
+    def compute_impulse(self, frame_time):
         delta_v = compute_total_impulse(self.mass, self.impulse)
-        self.velocity = compute_velocity_vector(self.velocity, delta_v)
+        self.velocity = compute_velocity_vector(
+            self.velocity, delta_v, frame_time)
         self.impulse.clear()
 
-    def update(self, frame_time):
+    def update(self, entities, frame_time):
+        self.apply_force(entities)
+        self.apply_impulse(entities)
         if self.impulse:
-            self.compute_impulse()
+            self.compute_impulse(frame_time)
         self.compute_force(frame_time)
 
     def draw(self):
--- util/setup.py
+++ util/setup.py
@@ -6,10 +6,12 @@ from lib.entity import Entity
 class Container:
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.entity = [Entity(screen, [500, 500], 10)]
+        entity1 = Entity(screen, [500, 401], 10, 20, 60)
+        entity2 = Entity(screen, [500, 1000], 100, 200, 600)
+        self.entity = [entity1, entity2]
 
     def update(self, frame_time):
         self.screen.fill(config.window_background)
         for e in self.entity:
             e.draw()
-            e.update(frame_time)
+            e.update(self.entity, frame_time)
